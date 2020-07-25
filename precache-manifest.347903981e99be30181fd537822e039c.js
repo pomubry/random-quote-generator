dd819e88aa31b133df581acb60d9f5c0e2954db6
pomubry <btaduran30@protonmail.com>
2020-07-25 17:21:04 +0800

    Updates

---
+++ precache-manifest.347903981e99be30181fd537822e039c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e56c4c86e04fc2f2c57d1a66547963",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "e5b23c5079d8b8d203d4",
+    "url": "/random-quote-generator/static/css/main.8dbf4724.chunk.css"
+  },
+  {
+    "revision": "147d99a732b14e576b7f",
+    "url": "/random-quote-generator/static/js/2.5b4ddbad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.5b4ddbad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b23c5079d8b8d203d4",
+    "url": "/random-quote-generator/static/js/main.18eba30c.chunk.js"
+  },
+  {
+    "revision": "a571005e16340c57e792",
+    "url": "/random-quote-generator/static/js/runtime-main.da7c7166.js"
+  },
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "/random-quote-generator/static/media/githubIcon.0a3a0978.svg"
+  },
+  {
+    "revision": "f0e9498d7f8444b1deb6408a944fb83c",
+    "url": "/random-quote-generator/static/media/quote.f0e9498d.svg"
+  }
+]);
