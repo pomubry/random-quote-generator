6e64c619451257709d3ddb8695d2139f5f90abea
pomubry <btaduran30@protonmail.com>
2021-02-08 18:23:23 +0800

    updated to React17, switched to func component, added react-icons

--- src/App.js
+++ src/App.js
@@ -1,146 +1,158 @@
-import React, { Component } from 'react';
-import quote from './quote.svg';
-import github from './githubIcon.svg';
-class App extends Component {
-  state = {
-    author: '',
-    content: '',
-    href: '',
-    quoteArr: [],
-    color: [
-      '#ED4C67',
-      '#F79F1F',
-      '#1289A7',
-      '#D980FA',
-      '#B53471',
-      '#EE5A24',
-      '#009432',
-      '#0652DD',
-      '#9980FA',
-      '#833471',
-      '#EA2027',
-      '#5758BB',
-    ],
-    activeColor: '',
-    opacity: 0,
-    transition: 'all 1s ease',
-  };
+import { useEffect, useState } from 'react';
 
-  componentDidMount() {
+// Icons
+import { FaQuoteLeft, FaGithub, FaTwitter } from 'react-icons/fa';
+import { BsArrowClockwise } from 'react-icons/bs';
+
+const color = [
+  '#ED4C67',
+  '#F79F1F',
+  '#1289A7',
+  '#D980FA',
+  '#B53471',
+  '#EE5A24',
+  '#009432',
+  '#0652DD',
+  '#9980FA',
+  '#833471',
+  '#EA2027',
+  '#5758BB',
+];
+
+// Function to get a random color
+const randomColor = () => color[Math.floor(Math.random() * color.length)];
+
+const App = () => {
+  const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
+  const [href, setHref] = useState('');
+  const [quoteArr, setQuoteArr] = useState([]);
+  const [activeColor, setActiveColor] = useState('');
+  const [opacity, setOpacity] = useState(0);
+  const [transition, setTransition] = useState('all 1s ease');
+
+  // Fetch quotes from a REST API & store it in state
+  useEffect(() => {
     fetch('https://api.quotable.io/quotes?limit=50&maxLength=200')
       .then((res) => res.json())
       .then((data) => {
-        const { color } = this.state;
         let index = Math.floor(Math.random() * data.results.length);
-        this.setState({
-          author: data.results[index].author,
-          content: data.results[index].content,
-          quoteArr: [...data.results],
-          activeColor: color[Math.floor(Math.random() * color.length)],
-          opacity: 1,
-        });
-      });
-  }
+        const { author, content } = data.results[index];
 
-  handleGenerate = (e) => {
+        setAuthor(author);
+        setContent(content);
+        setQuoteArr([...data.results]);
+        setActiveColor(randomColor);
+        setOpacity(1);
+      });
+  }, []);
+
+  // Get a random quote that was saved on the state.
+  const handleGenerate = (e) => {
     e.preventDefault();
-    const { quoteArr, color } = this.state;
     let index = Math.floor(Math.random() * quoteArr.length);
-    this.setState(
-      {
-        author: quoteArr[index].author,
-        content: quoteArr[index].content,
-        activeColor: color[Math.floor(Math.random() * color.length)],
-        opacity: 0,
-        transition: 'none',
-      },
-      () => {
-        setTimeout(() => {
-          this.setState({ opacity: 1, transition: 'all .25s ease' });
-        }, 250);
-      }
+    const { author, content } = quoteArr[index];
+
+    setAuthor(author);
+    setContent(content);
+    setActiveColor(randomColor);
+    setOpacity(0);
+    setTransition('none');
+
+    setTimeout(() => {
+      setOpacity(1);
+      setTransition('all .25s ease');
+    }, 250);
+  };
+
+  // Tweet Handler
+  const handleTweet = (e) => {
+    // Replace whitespace with %20
+    let authorStr = author.replace(/\s/g, '%20');
+    let contentStr = content.replace(/\s/g, '%20');
+
+    setHref(
+      `https://twitter.com/intent/tweet?text="${contentStr}"%20-%20${authorStr}`
     );
   };
 
-  handleTweet = (e) => {
-    let authorStr = this.state.author.replace(/\s/g, '%20');
-    let contentStr = this.state.content.replace(/\s/g, '%20');
-    console.log(this.state.href);
-    this.setState({
-      href: `https://twitter.com/intent/tweet?text="${contentStr}"%20-%20${authorStr}`,
-    });
+  // Only for setting initial background
+  const generalTransition = {
+    backgroundColor: activeColor,
+    transition: 'all 1s ease',
   };
 
-  render() {
-    const { activeColor, opacity, transition } = this.state;
-    const generalTransition = {
-      backgroundColor: activeColor,
-      transition: 'all 1s ease',
-    };
-    return (
-      <div className="App" style={generalTransition}>
-        <header>
-          <h1>Random Quote Generator</h1>
-        </header>
-        <div className="content-box" id="quote-box">
-          <div className="quote">
-            <h2
-              id="text"
-              style={{
-                color: activeColor,
-                transition: transition,
-                opacity: `${opacity}`,
-              }}
-              onTransitionEnd={this.handleTransition}
-            >
-              <img src={quote} alt="quote" className="quoteSVG" />{' '}
-              {this.state.content}
-            </h2>
-            <p
-              id="author"
-              style={{
-                color: activeColor,
-                transition: transition,
-                opacity: `${opacity}`,
-              }}
-            >
-              - {this.state.author}
-            </p>
-          </div>
-          <div className="buttons">
-            <button
-              onClick={this.handleGenerate}
-              id="new-quote"
-              style={generalTransition}
-            >
-              Generate a Random Quote
-            </button>
-            <a
-              href={this.state.href}
-              onClick={this.handleTweet}
-              rel="noopener noreferrer"
-              id="tweet-quote"
-              style={generalTransition}
-              target="_blank"
-            >
-              Tweet this Quote
-            </a>
-          </div>
-        </div>
-        <div className="credits">
-          <p>Designed and Coded By</p>
-          <a
-            href="https://github.com/pomubry"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div className="App" style={generalTransition}>
+      <header>
+        <h1>Random Quote Generator</h1>
+      </header>
+
+      {/* The entire Quote Box */}
+      <div className="content-box" id="quote-box">
+        <div className="quote">
+          {/* Quote */}
+          <h2
+            id="text"
+            style={{
+              color: activeColor,
+              transition: transition,
+              opacity: `${opacity}`,
+            }}
           >
-            <img src={github} alt="github icon" />
-            Bryan Taduran
+            <FaQuoteLeft />
+            {content}
+          </h2>
+
+          {/* Author */}
+          <p
+            id="author"
+            style={{
+              color: activeColor,
+              transition: transition,
+              opacity: `${opacity}`,
+            }}
+          >
+            - {author}
+          </p>
+        </div>
+
+        {/* Tweet and Random Quote Buttons */}
+        <div className="buttons">
+          <button
+            onClick={handleGenerate}
+            id="new-quote"
+            style={generalTransition}
+          >
+            <BsArrowClockwise /> Generate a Random Quote
+          </button>
+          <a
+            href={href}
+            onClick={handleTweet}
+            rel="noopener noreferrer"
+            id="tweet-quote"
+            style={generalTransition}
+            target="_blank"
+          >
+            <FaTwitter /> Tweet this Quote
           </a>
         </div>
       </div>
-    );
-  }
-}
+
+      {/* Profile Link */}
+      <div className="credits">
+        <p>Designed and Coded By</p>
+        <a
+          href="https://github.com/pomubry"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+          Bryan Taduran
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default App;
